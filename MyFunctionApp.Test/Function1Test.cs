84753444e6919b0a596ad599ced852f84cb00f9b
AdjeteGuru <justinadjev@hotmail.com>
2025-02-25 21:21:16 +0000

    feat: add test

--- MyFunctionApp.Test/Function1Test.cs
+++ MyFunctionApp.Test/Function1Test.cs
@@ -8,18 +8,26 @@ namespace MyFunctionApp.Test
     public class Function1Test
     {
         [Fact]
-        public void Run_WhenCalled_AssertTheCorrectType()
+        public async Task Run_WhenCalled_LogsMessage()
+        {
+            var loggerMock = new Mock<ILogger<Function1>>();
+            var function = new Function1(loggerMock.Object);
+            Func<Task> func = async () => await function.Run(new Mock<HttpRequest>().Object);
+        }
+
+        [Fact]
+        public async Task Run_WhenCalled_AssertTheCorrectType()
         {
             var function = new Function1(new Mock<ILogger<Function1>>().Object);
-            var result = function.Run(new Mock<HttpRequest>().Object);
+            var result = await function.Run(new Mock<HttpRequest>().Object);
             Assert.IsType<OkObjectResult>(result);
         }
 
         [Fact]
-        public void Run_WhenCalled_Trigger()
+        public async Task Run_WhenCalled_Trigger()
         {
             var function = new Function1(new Mock<ILogger<Function1>>().Object);
-            var result = function.Run(new Mock<HttpRequest>().Object);
+            var result = await function.Run(new Mock<HttpRequest>().Object);
             var resultObject = (OkObjectResult)result;
 
             Assert.Equal("Welcome to Azure Functions!", resultObject.Value);
--- MyFunctionApp/Function1.cs
+++ MyFunctionApp/Function1.cs
@@ -16,7 +16,7 @@ namespace MyFunctionApp
         }
 
         [Function("Function1")]
-        public IActionResult Run([HttpTrigger(Microsoft.Azure.Functions.Worker.AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(Microsoft.Azure.Functions.Worker.AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
             return new OkObjectResult("Welcome to Azure Functions!");
